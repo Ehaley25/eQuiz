cbb832daa23d4f7f94205eb2bff3d0eed1389dbf
Ehaley25 <elijahhaley99@gmail.com>
2023-02-08 17:16:17 -0700

    init

---
+++ eQuiz/src/assets/components/Questions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Questions(props){
+    return(
+        <>
+            <p>{props.test}</p>
+        </>
+    )
+}
+// pass props to "questions" that involve question & answers
+
+// I THINK
